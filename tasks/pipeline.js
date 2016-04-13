9935b6d6ff973192de54812ac2df68693ea4bde6
Enzo Toshio <enzo.toshiba@gmail.com>
2016-04-13 01:26:14 -0300

    adding js files to injection task

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -28,12 +28,18 @@ var jsFilesToInject = [
   // Load sails.io before everything else
   'js/dependencies/sails.io.js',
 
+  'js/dependencies/angular/angular.js',
+
+  'js/dependencies/angular-route/angular-route.js',
+
   // Dependencies like jQuery, or Angular are brought in here
-  'js/dependencies/**/*.js',
+  'js/dependencies/*.js',
 
   // All of the rest of your client-side js files
   // will be injected here in no particular order.
-  'js/**/*.js'
+  'js/*.js',
+
+  'js/controllers/*.js'
 ];
 
 
