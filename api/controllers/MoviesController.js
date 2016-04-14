54506d8ecce11b49b68e6e933e14e589aa798bcf
Enzo Toshio <enzo.toshiba@gmail.com>
2016-04-14 02:01:10 -0300

    doing login, signup and routes and controllers authentication

---
+++ api/controllers/MoviesController.js
@@ -0,0 +1,11 @@
+/**
+ * MoviesController
+ *
+ * @description :: Server-side logic for managing movies
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/controllers/UserController.js
@@ -0,0 +1,12 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	home: function(req, res) {
+		res.view('homepage');
+	}
+};
